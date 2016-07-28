c1f91ac7198067bc0cfc46a711ae9486aa13faa8
Jaalah Ramos <jaalah.ramos@espn.com>
2016-07-28 09:24:53 -0400

    Updates to template

--- src/js/main.js
+++ src/js/main.js
@@ -18,10 +18,8 @@ requirejs.config({
 });
 
 require([
-  // Load our app module and pass it to our definition function
-  'app',
-  'book'
-], function(App, Book){
+  'app' // Load our app module and pass it to our definition function
+], function( App, Book ) {
   // The "app" dependency is passed in as "App"
   // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
 });
